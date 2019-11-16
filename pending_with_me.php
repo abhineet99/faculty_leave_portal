3793f227ee345de4587adc5dca062daf4ff19033
abhineet9 <2017csb1062@iitrpr.ac.in>
2019-11-16 15:47:33 +0530

    pending template ready

--- pending_with_me.php
+++ pending_with_me.php
@@ -25,7 +25,14 @@
             $dept=pg_query($query);
             $dept=pg_fetch_row($dept);
             $dept=$dept[0];
-            $query="SELECT facult.leave.leave_id,facult.leave.sender_id,facult.leave.doa from facult.faculty where email='$email";
+            $query="SELECT facult.leave.leave_id,facult.leave.sender_id,facult.leave.doa from facult.faculty,facult.leave where facult.faculty.email=facult.leave_id.sender_id AND facult.faculty.department='$dept' AND facult.leave.pending_id='hod' AND facult.leave.status=0";
+
+            //INSERT CODE to display all these parameters(comments)
+        }
+        else{
+            //no need to fetch department in this case
+            $query="SELECT facult.leave.leave_id,facult.leave.sender_id,facult.leave.doa from facult.leave where facult.leave.pending_id='$post' AND facult.leave.status=0";
+            //print all these results and give link to leave details (comments)
         }
     }
 	?>
